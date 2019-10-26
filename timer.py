a9d16c8548c4d051f66847f2c018d07401ed710f
Svitlana Zaster <szaster@protonmail.com>
2019-10-26 17:13:48 -0500

    explore datetime modules

---
+++ timer.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from datetime import timedelta
+from datetime import date
+
+t = timedelta(days=10, hours=3)
+today = datetime.today().date()
+
+print(today)
+
+eta = timedelta(hours=1)
+timer = today + eta
+
+print("The timer is up at ", timer)
+
+anniversary = date(2019, 11, 11)
+
+print("Our anniversary is on ", anniversary)
+
+daysleft = (anniversary - today).days
+
+print(daysleft)
