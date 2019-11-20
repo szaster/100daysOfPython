86f87a8e491cd595fbb6a70a13c38c98fdddda1c
Svitlana Zaster <szaster@protonmail.com>
2019-11-20 15:29:08 -0600

    Writing conditional statements for BG below target

--- time_exercise/deltat.py
+++ time_exercise/deltat.py
@@ -3,18 +3,18 @@ import datetime
 
 def eatingtimer():
     global minutes
-    targetBG = 120
+    targetBG = 110
     print(
         'Type your current BG.')
     BG = input()  # Type your current BG
-    print ("My current BG is", BG)
+    print("My current BG is", BG)
     print('Type food GI: "high", "moderate", or "low"')
-    GI = input() # Type food GI
+    GI = input()  # Type food GI
     print("The food's GI is", GI)
     now = datetime.datetime.now()
     print("Current time is: ", datetime.datetime.now())
 
-    #BG is above target block
+    # BG is above target block
     if int(BG) > targetBG and GI == "high":
         dthigh = datetime.timedelta(minutes=30)
         deltahigh = now + dthigh
@@ -26,15 +26,43 @@ def eatingtimer():
     if int(BG) > targetBG and GI == "low":
         dthigh = datetime.timedelta(minutes=10)
         deltahigh = now + dthigh
-    print("My current BG is ", BG, "at time ", now, "I can eat in ", dthigh, "minutes at", deltahigh)
-    #try:
-       # while True:
-            #now = datetime.datetime.now()
-            #print(now)
-            #delta15 = datetime.timedelta(minutes=15)
-            #delta20 = datetime.timedelta(minutes=20)
-            #print("Start eating in ", delta15, "minutes is ", now + delta15)
-   # except KeyboardInterrupt:
-        # Handle the Ctrl+C exception to keep its error message from displaying.
-       # print('\nDone.')
+        print("My current BG is ", BG, "at time ", now, "I can eat in ", dthigh, "minutes at", deltahigh)
 
+    # BG is at target block
+    if int(BG) == targetBG and GI == "high":
+        dthigh = datetime.timedelta(minutes=20)
+        deltahigh = now + dthigh
+        print("My current BG is ", BG, "at time ", now, "I can eat in ", dthigh, "minutes at", deltahigh)
+    if int(BG) == targetBG and GI == "moderate":
+        dthigh = datetime.timedelta(minutes=10)
+        deltahigh = now + dthigh
+        print("My current BG is ", BG, "at time ", now, "I can eat in ", dthigh, "minutes at", deltahigh)
+    if int(BG) == targetBG and GI == "low":
+        dthigh = datetime.timedelta(minutes=10)
+        deltahigh = now + dthigh
+        print("My current BG is ", BG, "at time ", now, "I can eat right now and bolus in 10 minutes at", deltahigh)
+
+    # BG is below target block
+    if int(BG) < targetBG and GI == "high":
+        dthigh = datetime.timedelta(minutes=5)
+        deltahigh = now + dthigh
+        print("My current BG is ", BG, "at time ", now, "I can eat in ", dthigh, "minutes at", deltahigh)
+    if int(BG) < targetBG and GI == "moderate":
+        dthigh = datetime.timedelta(minutes=5)
+        deltahigh = now + dthigh
+        print("My current BG is ", BG, "at time ", now, "I can eat right now and bolus in 5 minutes at", deltahigh)
+    if int(BG) < targetBG and GI == "low":
+        dthigh = datetime.timedelta(minutes=20)
+        deltahigh = now + dthigh
+        print("My current BG is ", BG, "at time ", now, "I can eat right now and bolus in 20 minutes at", deltahigh)
+
+    # try:
+    # while True:
+    # now = datetime.datetime.now()
+    # print(now)
+    # delta15 = datetime.timedelta(minutes=15)
+    # delta20 = datetime.timedelta(minutes=20)
+    # print("Start eating in ", delta15, "minutes is ", now + delta15)
+# except KeyboardInterrupt:
+# Handle the Ctrl+C exception to keep its error message from displaying.
+# print('\nDone.')
