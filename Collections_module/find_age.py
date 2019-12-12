4d3b24c6be5023be872f8741c43df48ae47a14e2
Svitlana Zaster <szaster@protonmail.com>
2019-12-12 13:14:31 -0600

    bug fix

--- Collections_module/find_age.py
+++ Collections_module/find_age.py
@@ -17,8 +17,6 @@ def get_person_age(name):
     is_found = False
     for group in groups:
         if name in group:
-            # name = group1.keys()
-            # age = group1.values()
             print(name + ' is ' + str(group[name]) + ' years old.')
             is_found = True
             break
@@ -27,9 +25,30 @@ def get_person_age(name):
 
 while True:
     print("Enter a name: (blank to quit)")
-    name = input()
+    name = str.casefold(input())
     if name == '':
         break
 
     if not get_person_age(name):
         print("Name is not found")
+
+
+    def test_regular_name():
+        assert get_person_age('tim') == 30
+        assert get_person_age('helen') == 26
+        assert get_person_age('otto') == 44
+
+    def test_case_insensitive_lookup():
+        assert get_person_age('Tim') == 30
+        assert get_person_age('BOB') == 17
+        assert get_person_age('BrEnDa') == 17
+
+    def test_name_not_found():
+        assert get_person_age('timothy') == NOT_FOUND
+        assert get_person_age(None) == NOT_FOUND
+        assert get_person_age(False) == NOT_FOUND
+        assert get_person_age(-1) == NOT_FOUND
+
+    def test_duplicate_name():
+        assert get_person_age('thomas') == 46
+        assert get_person_age('ana') == 26
