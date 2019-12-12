2b557d6bec0eca55d421f6d328276dabda44abb5
Svitlana Zaster <szaster@protonmail.com>
2019-12-12 14:51:23 -0600

    compare  a value from a dict. if-else exercise

---
+++ intro_py_bites/bite101_fstring_if_else.py
@@ -0,0 +1,27 @@
+MIN_DRIVING_AGE = 18
+group = {'tim': 17, 'bob': 18, 'ana': 24}
+
+
+def allowed_driving(name):
+    """Print '{name} is allowed to drive' or '{name} is not allowed to drive'
+       checking the passed in age against the MIN_DRIVING_AGE constant
+       """
+    is_found = False
+    if name in group:
+            if group[name] >= MIN_DRIVING_AGE:
+                print(name + " is allowed to drive")
+                is_found = True
+            else:
+                print(name + " is not allowed to drive")
+                is_found = True
+    return is_found
+
+while True:
+    print("Enter a name: (blank to quit)")
+    name = str.casefold(input())
+    if name == '':
+        break
+
+    if not allowed_driving(name):
+        print("Name is not found")
+
