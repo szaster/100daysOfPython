b94c6f68b221d2be90c26c06e53553a9e4151bd0
Svitlana Zaster <szaster@protonmail.com>
2019-12-17 17:23:07 -0600

    bug fix bite 101

--- intro_py_bites/bite101_test_practice.py
+++ intro_py_bites/bite101_test_practice.py
@@ -3,9 +3,9 @@ group = {'tim': 17, 'bob': 18, 'ana': 24}
 
 
 #def allowed_driving(thisgroup):
-    """Print '{name} is allowed to drive' or '{name} is not allowed to drive'
-       checking the passed in age against the MIN_DRIVING_AGE constant
-       """
+   # """Print '{name} is allowed to drive' or '{name} is not allowed to drive'
+      # checking the passed in age against the MIN_DRIVING_AGE constant
+    #   """
  #   for name in thisgroup:
   #      if thisgroup[name] >= MIN_DRIVING_AGE:
    #         print(name + " is allowed to drive")
@@ -17,4 +17,4 @@ def allowed_driving(name, age):
     is_allowed = 'is allowed' if age >= MIN_DRIVING_AGE else 'is not allowed'
     print(f'{name} {is_allowed} to drive')
 
-#allowed_driving('tim', 17) ?????? (group?)
+allowed_driving('kok' , 89)
